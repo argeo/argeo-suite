fed56d58f4c9e48b6d8b0edee36928294ae31264
Stéphane Leroy <sleroy@argeo.org>
2020-11-02 09:49:12 +0000

    add style for argeo publishing

--- publishing/org.argeo.publishing.ui/src/org/argeo/publishing/ui/PublishingStyle.java
+++ publishing/org.argeo.publishing.ui/src/org/argeo/publishing/ui/PublishingStyle.java
@@ -4,7 +4,14 @@ import org.argeo.cms.ui.util.CmsStyle;
 
 /** Publishing styles. */
 public enum PublishingStyle implements CmsStyle {
-	page;
+	
+	page,coverTitle,coverSubTitle,coverTagline,bannerLine1,bannerLine2,
+
+	// text style
+	title,subTitle,chapo,para,sectionTitle,subSectionTitle,
+	
+	// composite style
+	framed,line;
 
 	@Override
 	public String getClassPrefix() {
