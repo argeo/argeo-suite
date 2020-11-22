1b658b3ddd1563719c84968e90159ba604672af8
Mathieu Baudier <mbaudier@argeo.org>
2020-11-22 19:32:43 +0100

    Introduce admin UI.

---
+++ org.argeo.suite.ui/src/org/argeo/suite/ui/SuiteUserUiProvider.java
@@ -0,0 +1,18 @@
+package org.argeo.suite.ui;
+
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+
+import org.argeo.cms.ui.CmsUiProvider;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+
+public class SuiteUserUiProvider implements CmsUiProvider {
+
+	@Override
+	public Control createUi(Composite parent, Node context) throws RepositoryException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
