c186c05d53ea978b7bc70892a8d307cbe5a123fa
Mathieu Baudier <mbaudier@argeo.org>
2021-05-25 06:28:39 +0200

    Improve content layer.

--- publishing/org.argeo.publishing.ui/src/org/argeo/publishing/ui/DocumentUiProvider.java
+++ publishing/org.argeo.publishing.ui/src/org/argeo/publishing/ui/DocumentUiProvider.java
@@ -2,6 +2,7 @@ package org.argeo.publishing.ui;
 
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
+import javax.jcr.nodetype.NodeType;
 
 import org.argeo.cms.ui.CmsEditable;
 import org.argeo.cms.ui.CmsUiProvider;
@@ -12,6 +13,7 @@ import org.argeo.docbook.DbkType;
 import org.argeo.docbook.ui.AbstractDbkViewer;
 import org.argeo.docbook.ui.DocumentTextEditor;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.browser.Browser;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 
@@ -30,7 +32,17 @@ public class DocumentUiProvider implements CmsUiProvider {
 			page.setLayout(CmsUiUtils.noSpaceGridLayout());
 
 			AbstractDbkViewer dbkEditor = new DocumentTextEditor(page, SWT.NONE, textNode, cmsEditable);
+			return page;
 
+		} else if (context.isNodeType(NodeType.NT_FILE)) {
+			String fileName = context.getName();
+			if (fileName.endsWith(".pdf")) {
+				Browser browser = new Browser(parent, SWT.NONE);
+				String dataPath = CmsUiUtils.getDataPath(context);
+				browser.setUrl(dataPath);
+				browser.setLayoutData(CmsUiUtils.fillAll());
+				return browser;
+			}
 		}
 		return null;
 	}
