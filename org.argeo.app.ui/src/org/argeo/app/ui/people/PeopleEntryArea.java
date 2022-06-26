3505709f50eec3a940e82c6aac7bc2d4bd1dec65
Mathieu Baudier <mbaudier@argeo.org>
2022-06-26 10:47:24 +0200

    Improve people entry area.

--- org.argeo.app.ui/src/org/argeo/app/ui/people/PeopleEntryArea.java
+++ org.argeo.app.ui/src/org/argeo/app/ui/people/PeopleEntryArea.java
@@ -1,97 +1,169 @@
 package org.argeo.app.ui.people;
 
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
 
-import org.argeo.app.api.SuiteRole;
+import org.argeo.api.acr.Content;
+import org.argeo.api.acr.ContentRepository;
+import org.argeo.api.acr.ContentSession;
+import org.argeo.api.cms.ux.CmsIcon;
+import org.argeo.api.cms.ux.CmsView;
 import org.argeo.app.ui.SuiteEvent;
 import org.argeo.app.ui.SuiteIcon;
 import org.argeo.app.ui.dialogs.NewUserWizard;
 import org.argeo.cms.CmsUserManager;
+import org.argeo.cms.acr.ContentUtils;
+import org.argeo.cms.auth.CmsRole;
+import org.argeo.cms.auth.CurrentUser;
+import org.argeo.cms.auth.UserAdminUtils;
+import org.argeo.cms.jcr.acr.JcrContent;
 import org.argeo.cms.jface.dialog.CmsWizardDialog;
 import org.argeo.cms.swt.CmsSwtTheme;
 import org.argeo.cms.swt.CmsSwtUtils;
 import org.argeo.cms.swt.Selected;
+import org.argeo.cms.swt.acr.SwtUiProvider;
+import org.argeo.cms.swt.widgets.SwtHierarchicalPart;
+import org.argeo.cms.swt.widgets.SwtTabularPart;
 import org.argeo.cms.ui.CmsUiProvider;
+import org.argeo.cms.ux.widgets.AbstractHierarchicalPart;
+import org.argeo.cms.ux.widgets.Column;
+import org.argeo.cms.ux.widgets.DefaultTabularPart;
+import org.argeo.cms.ux.widgets.HierarchicalPart;
+import org.argeo.osgi.useradmin.UserDirectory;
+import org.argeo.util.directory.HierarchyUnit;
 import org.argeo.util.naming.LdapAttrs;
-import org.eclipse.jface.viewers.ColumnLabelProvider;
-import org.eclipse.jface.viewers.DoubleClickEvent;
-import org.eclipse.jface.viewers.IDoubleClickListener;
-import org.eclipse.jface.viewers.ISelectionChangedListener;
-import org.eclipse.jface.viewers.IStructuredContentProvider;
-import org.eclipse.jface.viewers.SelectionChangedEvent;
-import org.eclipse.jface.viewers.TableViewer;
-import org.eclipse.jface.viewers.TableViewerColumn;
-import org.eclipse.jface.viewers.Viewer;
+import org.argeo.util.naming.LdapObjs;
 import org.eclipse.jface.window.Window;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.SashForm;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.ToolBar;
 import org.eclipse.swt.widgets.ToolItem;
+import org.osgi.service.useradmin.Role;
 import org.osgi.service.useradmin.User;
 
 /** Entry to the admin area. */
-public class PeopleEntryArea implements CmsUiProvider {
+public class PeopleEntryArea implements SwtUiProvider, CmsUiProvider {
 
 	private CmsUserManager cmsUserManager;
 
+	private ContentRepository contentRepository;
+
 	@Override
-	public Control createUi(Composite parent, Node context) throws RepositoryException {
+	public Control createUiPart(Composite parent, Content context) {
 		CmsSwtTheme theme = CmsSwtUtils.getCmsTheme(parent);
+		CmsView cmsView = CmsSwtUtils.getCmsView(parent);
 		parent.setLayout(new GridLayout());
-		TableViewer usersViewer = new TableViewer(parent);
-		usersViewer.setContentProvider(new UsersContentProvider());
 
-		TableViewerColumn idCol = new TableViewerColumn(usersViewer, SWT.NONE);
-		idCol.getColumn().setWidth(70);
-		idCol.setLabelProvider(new ColumnLabelProvider() {
+		ContentSession contentSession = contentRepository.get();
+		SashForm sashForm = new SashForm(parent, SWT.VERTICAL);
+		CmsSwtUtils.fill(sashForm);
 
-			@Override
-			public String getText(Object element) {
+		// MODEL
+//		List<UserDirectory> directories = new ArrayList<>();
+//		// List<User> orgs = cmsUserManager.listGroups(null, true, false);
+//		for (UserDirectory directory : cmsUserManager.getUserDirectories()) {
+//			if (CurrentUser.implies(CmsRole.userAdmin, directory.getContext())) {
+//				directories.add(directory);
+//			}
+//
+//		}
 
-				return getUserProperty(element, LdapAttrs.uid.name());
-			}
-		});
-
-		TableViewerColumn givenNameCol = new TableViewerColumn(usersViewer, SWT.NONE);
-		givenNameCol.getColumn().setWidth(150);
-		givenNameCol.setLabelProvider(new ColumnLabelProvider() {
-
-			@Override
-			public String getText(Object element) {
-
-				return getUserProperty(element, LdapAttrs.givenName.name());
-			}
-		});
-
-		TableViewerColumn snCol = new TableViewerColumn(usersViewer, SWT.NONE);
-		snCol.getColumn().setWidth(150);
-		snCol.setLabelProvider(new ColumnLabelProvider() {
+		// VIEW
+		HierarchicalPart<HierarchyUnit> hierarchyPart = new AbstractHierarchicalPart<>() {
 
 			@Override
-			public String getText(Object element) {
+			public List<HierarchyUnit> getChildren(HierarchyUnit parent) {
+				List<HierarchyUnit> visible = new ArrayList<>();
+				if (parent != null) {
+					for (HierarchyUnit hu : parent.getDirectHierarchyUnits(true)) {
+						if (CurrentUser.implies(CmsRole.userAdmin, hu.getContext())) {
+							visible.add(hu);
+						}
+					}
+				} else {
+					for (UserDirectory directory : cmsUserManager.getUserDirectories()) {
+						if (CurrentUser.implies(CmsRole.userAdmin, directory.getContext())) {
+							visible.add(directory);
+						}
 
-				return getUserProperty(element, LdapAttrs.sn.name());
+					}
+				}
+				return visible;
 			}
-		});
-
-		TableViewerColumn mailCol = new TableViewerColumn(usersViewer, SWT.NONE);
-		mailCol.getColumn().setWidth(400);
-		mailCol.setLabelProvider(new ColumnLabelProvider() {
 
 			@Override
-			public String getText(Object element) {
-
-				return getUserProperty(element, LdapAttrs.mail.name());
+			public String getText(HierarchyUnit model) {
+				return model.getHierarchyUnitName();
 			}
-		});
 
+		};
+		SwtHierarchicalPart<HierarchyUnit> directoriesView = new SwtHierarchicalPart<>(sashForm, SWT.NONE,
+				hierarchyPart);
+
+		DefaultTabularPart<HierarchyUnit, Content> usersPart = new DefaultTabularPart<>() {
+
+			@Override
+			protected List<Content> asList(HierarchyUnit hu) {
+				List<Content> roles = new ArrayList<>();
+				UserDirectory ud = (UserDirectory) hu.getDirectory();
+				for (HierarchyUnit directChild : hu.getDirectHierarchyUnits(false)) {
+					if (!directChild.isFunctional()) {
+						for (Role r : ud.getHierarchyUnitRoles(directChild, null, false)) {
+							Content content = ContentUtils.roleToContent(cmsUserManager, contentSession, r);
+							// if (r instanceof Person || r instanceof Organization)
+							if (content.hasContentClass(LdapObjs.inetOrgPerson.qName(), LdapObjs.organization.qName()))
+								roles.add(content);
+						}
+					}
+				}
+				return roles;
+			}
+		};
+		usersPart.addColumn(new Column<Content>() {
+
+			@Override
+			public String getText(Content role) {
+				if (role.isContentClass(LdapObjs.inetOrgPerson.qName()))
+					return UserAdminUtils.getUserDisplayName(role.adapt(User.class));
+				else if (role.isContentClass(LdapObjs.organization.qName()))
+					return role.attr(LdapAttrs.o.qName());
+				else if (role.isContentClass(LdapObjs.groupOfNames.qName()))
+					return role.attr(LdapAttrs.cn.qName());
+				else
+					return null;
+			}
+
+			@Override
+			public CmsIcon getIcon(Content role) {
+				if (role.isContentClass(LdapObjs.inetOrgPerson.qName()))
+					return SuiteIcon.person;
+				else if (role.isContentClass(LdapObjs.organization.qName()))
+					return SuiteIcon.organisation;
+				else if (role.isContentClass(LdapObjs.groupOfNames.qName()))
+					return SuiteIcon.group;
+				else
+					return null;
+			}
+
+			@Override
+			public int getWidth() {
+				return 300;
+			}
+
+		});
+		usersPart.addColumn((Column<Content>) (role) -> role.attr(LdapAttrs.mail.qName()));
+
+		SwtTabularPart<HierarchyUnit, Content> usersView = new SwtTabularPart<>(sashForm, SWT.NONE, usersPart);
+
+		// toolbar
 		Composite bottom = new Composite(parent, SWT.NONE);
 		bottom.setLayoutData(CmsSwtUtils.fillWidth());
 		bottom.setLayout(CmsSwtUtils.noSpaceGridLayout());
@@ -103,30 +175,30 @@ public class PeopleEntryArea implements CmsUiProvider {
 		deleteItem.setImage(theme.getSmallIcon(SuiteIcon.delete));
 		ToolItem addItem = new ToolItem(bottomToolBar, SWT.FLAT);
 		addItem.setImage(theme.getSmallIcon(SuiteIcon.add));
-		usersViewer.addDoubleClickListener(new IDoubleClickListener() {
 
-			@Override
-			public void doubleClick(DoubleClickEvent event) {
-				User user = (User) usersViewer.getStructuredSelection().getFirstElement();
-				if (user != null) {
-//					Node userNode = getOrCreateUserNode(user, context);
-					CmsSwtUtils.getCmsView(parent).sendEvent(SuiteEvent.openNewPart.topic(),
-							SuiteEvent.eventProperties(user));
-				}
+		sashForm.setWeights(new int[] { 30, 70 });
 
+		// CONTROLLER
+		hierarchyPart.onSelected((o) -> {
+			if (o instanceof HierarchyUnit) {
+				usersPart.setInput((HierarchyUnit) o);
 			}
 		});
-		usersViewer.addSelectionChangedListener(new ISelectionChangedListener() {
-			public void selectionChanged(SelectionChangedEvent event) {
-				User user = (User) usersViewer.getStructuredSelection().getFirstElement();
-				if (user != null) {
-//					Node userNode = getOrCreateUserNode(user, context);
-					CmsSwtUtils.getCmsView(parent).sendEvent(SuiteEvent.refreshPart.topic(),
-							SuiteEvent.eventProperties(user));
-					deleteItem.setEnabled(true);
-				} else {
-					deleteItem.setEnabled(false);
-				}
+
+		usersPart.onSelected((o) -> {
+			Content user = (Content) o;
+			if (user != null) {
+				cmsView.sendEvent(SuiteEvent.refreshPart.topic(), SuiteEvent.eventProperties(user));
+				deleteItem.setEnabled(true);
+			} else {
+				deleteItem.setEnabled(false);
+			}
+		});
+
+		usersPart.onAction((o) -> {
+			Content user = (Content) o;
+			if (user != null) {
+				cmsView.sendEvent(SuiteEvent.openNewPart.topic(), SuiteEvent.eventProperties(user));
 			}
 		});
 
@@ -140,44 +212,37 @@ public class PeopleEntryArea implements CmsUiProvider {
 			}
 		});
 
-		usersViewer.getTable().setLayoutData(CmsSwtUtils.fillAll());
-		usersViewer.setInput(cmsUserManager);
+		directoriesView.refresh();
+//		usersView.refresh();
 
-		return usersViewer.getTable();
+		return sashForm;
 	}
 
-//	private Node getOrCreateUserNode(User user, Node context) {
-//		return JcrUtils.mkdirs(Jcr.getSession(context),
-//				"/" + EntityType.user.name() + "/" + getUserProperty(user, LdapAttrs.uid.name()),
-//				EntityType.user.get());
+//	static String getProperty(Role role, LdapAttrs attr) {
+//		Object value = role.getProperties().get(attr.name());
+//		return value != null ? value.toString() : null;
 //	}
 
-	private String getUserProperty(Object element, String key) {
-		Object value = ((User) element).getProperties().get(key);
-		return value != null ? value.toString() : null;
-	}
-
-	class UsersContentProvider implements IStructuredContentProvider {
-
-		@Override
-		public Object[] getElements(Object inputElement) {
-			CmsUserManager cum = (CmsUserManager) inputElement;
-			Set<User> users = cum.listUsersInGroup(SuiteRole.coworker.dn(), null);
-			return users.toArray();
-		}
-
-		@Override
-		public void dispose() {
-		}
-
-		@Override
-		public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
-		}
-
-	}
+//	private boolean isOrganisation(Role role) {
+//		String[] objectClasses = role.getProperties().get(LdapAttrs.objectClasses.name()).toString().split("\\n");
+//		for (String objectClass : objectClasses) {
+//			if (LdapObjs.organization.name().equalsIgnoreCase(objectClass))
+//				return true;
+//		}
+//		return false;
+//	}
 
 	public void setCmsUserManager(CmsUserManager cmsUserManager) {
 		this.cmsUserManager = cmsUserManager;
+	}
+
+	@Override
+	public Control createUi(Composite parent, Node context) throws RepositoryException {
+		return createUiPart(parent, JcrContent.nodeToContent(context));
+	}
+
+	public void setContentRepository(ContentRepository contentRepository) {
+		this.contentRepository = contentRepository;
 	}
 
 }
