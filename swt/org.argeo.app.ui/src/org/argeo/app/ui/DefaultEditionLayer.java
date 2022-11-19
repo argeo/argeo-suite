745663105f60541454e75d4e4e31153698ebd92e
Mathieu <mbaudier@argeo.org>
2022-11-19 13:09:32 +0100

    Make single tab title configurable

--- swt/org.argeo.app.ui/src/org/argeo/app/ui/DefaultEditionLayer.java
+++ swt/org.argeo.app.ui/src/org/argeo/app/ui/DefaultEditionLayer.java
@@ -31,6 +31,7 @@ public class DefaultEditionLayer implements SuiteLayer {
 	private boolean fixedEntryArea = false;
 	private boolean singleTab = false;
 	private Localized title = null;
+	private Localized singleTabTitle = null;
 
 	@Override
 	public Control createUiPart(Composite parent, Content context) {
@@ -161,6 +162,29 @@ public class DefaultEditionLayer implements SuiteLayer {
 				title = new Localized.Untranslated(titleStr);
 			}
 		}
+
+		String singleTabTitleStr = (String) properties.get(SuiteLayer.Property.singleTabTitle.name());
+		if (singleTabTitleStr != null) {
+			if (singleTabTitleStr.startsWith("%")) {
+				singleTabTitle = new Localized() {
+
+					@Override
+					public String name() {
+						return singleTabTitleStr;
+					}
+
+					@Override
+					public ClassLoader getL10nClassLoader() {
+						return bundleContext != null
+								? bundleContext.getBundle().adapt(BundleWiring.class).getClassLoader()
+								: getClass().getClassLoader();
+					}
+				};
+			} else {
+				singleTabTitle = new Localized.Untranslated(singleTabTitleStr);
+			}
+		}
+
 	}
 
 	public void destroy(BundleContext bundleContext, Map<String, String> properties) {
@@ -182,6 +206,8 @@ public class DefaultEditionLayer implements SuiteLayer {
 	SwtTabbedArea createTabbedArea(Composite parent, CmsSwtTheme theme) {
 		SwtTabbedArea tabbedArea = new SwtTabbedArea(parent, SWT.NONE);
 		tabbedArea.setSingleTab(singleTab);
+		if (singleTabTitle != null)
+			tabbedArea.setSingleTabTitle(singleTabTitle.lead());
 		tabbedArea.setBodyStyle(SuiteStyle.mainTabBody.style());
 		tabbedArea.setTabStyle(SuiteStyle.mainTab.style());
 		tabbedArea.setTabSelectedStyle(SuiteStyle.mainTabSelected.style());
