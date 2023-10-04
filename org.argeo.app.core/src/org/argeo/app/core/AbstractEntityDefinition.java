3283994ed0bb41099776e8f6ec9b3fbcbc767a84
Mathieu Baudier <mbaudier@argeo.org>
2023-10-04 09:14:08 +0200

    Deprecate JCR entity definition

---
+++ org.argeo.app.core/src/org/argeo/app/core/AbstractEntityDefinition.java
@@ -0,0 +1,14 @@
+package org.argeo.app.core;
+
+import org.argeo.app.api.EntityDefinition;
+
+public abstract class AbstractEntityDefinition implements EntityDefinition {
+
+	public void init() {
+
+	}
+
+	public void destroy() {
+
+	}
+}
